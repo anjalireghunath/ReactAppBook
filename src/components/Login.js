6d2a1340670050ae7cdd433601f648a5bce61a54
anjalireghunath <anjalireghu98@gmail.com>
2022-05-19 22:48:40 +0530

    login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,23 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+    var [username,setUsername]=useState("")
+    var [password,setPassword]=useState("")
+    const subData=()=>{
+        const data={"username":username,"password":password}
+        console.log(data)
+    }
   return (
     <div>
 
 <div class="container">
     <div class="row">
         <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-            <div class="row">
+            <div class="row g-3">
                 <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label for="" class="form-label">UserName</label>
-                    <input type="text" class="form-control"/>
+                    <input onChange={(y)=>{setUsername(y.target.value)}} type="text" class="form-control"/>
                 </div>
                 <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label for="" class="form-label">Password</label>
-                    <input type="text" class="form-control"/>
+                    <input onChange={(y)=>{setPassword(y.target.value)}} type="text" class="form-control"/>
                 </div>
                 <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <button class="btn btn-primary">LOGIN</button>
+                    <button onClick={subData} class="btn btn-primary">LOGIN</button>
                 </div>
                 <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <a href="register.js">New user click here</a>
